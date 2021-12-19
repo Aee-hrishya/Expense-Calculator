312003b78851d6a505d1e86794c3572207a087ad
hrishikesh <hshinde166@gmail.com>
2021-12-20 01:13:20 +0530

    We created a separate component for the date and made the changes accordingly in the other files

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,19 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"; //Importing the css file
 
-function ExpenseItem (){
-    //Below is some basic javascript to demonstrate how data can be added dynamically
-    const expenseDate = new Date();
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 14000;
-    
-    return(
+//Now we pass props here as parameter so that we can access the key value pairs inside it and make use of them
+function ExpenseItem(props) {
+
+    return (
 
         <div className="expense-item">
             {/* We can add data dynamically by using the curly braces */}
-            <div>{expenseDate.toDateString(12-19-2021)}</div>  
+            {/* Now we can separate this date divs and create a separate component for this and then use them here, so we created the ExpenseDate component */}
+            <ExpenseDate date={props.date}/> {/* we can do this when the component we are using has no text between it */}
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
 
         </div>
