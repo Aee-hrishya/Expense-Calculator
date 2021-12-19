c953afb1f316662ce99af893533533e52e65c519
hrishikesh <hshinde166@gmail.com>
2021-12-20 02:12:27 +0530

    Created the Expenses component which imports the ExpenseItems component

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -9,7 +9,7 @@ function ExpenseItem(props) {
         <div className="expense-item">
             {/* We can add data dynamically by using the curly braces */}
             {/* Now we can separate this date divs and create a separate component for this and then use them here, so we created the ExpenseDate component */}
-            <ExpenseDate date={props.date}/> {/* we can do this when the component we are using has no text between it */}
+            <ExpenseDate date={props.date} /> {/* we can do this when the component we are using has no text between it */}
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
