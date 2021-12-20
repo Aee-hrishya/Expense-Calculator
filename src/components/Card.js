58a4079f8b20af1e663dd86d70d241a9a4ecbf4d
hrishikesh <hshinde166@gmail.com>
2021-12-20 20:49:56 +0530

    Created a custom wrapper component named Card so that we can apply common styles to the other components without repeating ourselves

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+
+function Card(props){
+
+    const classes = "card " + props.className; //Here we tweaked our classname a little bit as we wanted to also add the styling that the other components have so we concatenated it to our own class and made a custom class so that we can use it.
+
+    return(
+        <div className={classes}>{props.children}</div> // props.children is a special prop that react has and it is used to create wrapper components and now we can use this component to to wrap some JSX code inside it.Basically props.children is everything that the component has inside its opening and closing tags, so we can use them without repeating ourselves here in this case we just apply common styles to them
+    );
+}
+
+export default Card;
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,14 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css"; //Importing the css file
+
 
 //Now we pass props here as parameter so that we can access the key value pairs inside it and make use of them
 function ExpenseItem(props) {
 
     return (
 
-        <div className="expense-item">
+        <Card className="expense-item"> {/* here Card is the custom component wrapper which is used to wrap the other components so that we can apply styles to them which are repetitive for some of them */}
             {/* We can add data dynamically by using the curly braces */}
             {/* Now we can separate this date divs and create a separate component for this and then use them here, so we created the ExpenseDate component */}
             <ExpenseDate date={props.date} /> {/* we can do this when the component we are using has no text between it */}
@@ -15,7 +17,7 @@ function ExpenseItem(props) {
                 <div className="expense-item__price">{props.amount}</div>
             </div>
 
-        </div>
+        </Card>
     );
 }
 
