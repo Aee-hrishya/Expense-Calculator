231dd6c18173cc2e23d2e86ff397bd34d2f33395
hrishikesh <hshinde166@gmail.com>
2021-12-23 00:18:06 +0530

    Changed the min and max attributes of the input tag so that the proper years can be selected

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -60,7 +60,7 @@ const ExpenseForm = (props) => {
                     </div>
                     <div className="new-expense__control">
                         <label>Date</label>
-                        <input type="date" value={enteredDate} min="2021-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                        <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                     </div>
                </div>
                <div className="new-expense__actions">
